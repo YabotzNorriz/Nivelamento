6054f8cb3b5d61a70d9444c5e0ff5cf5f56902a4
João Pedro A.P.P. Barbosa <jpandradepaesdev@outlook.com.br>
2025-03-25 10:41:08 -0300

    Updated transformacao_dados.py

--- transformacao_dados.py
+++ transformacao_dados.py
@@ -1,2 +1,23 @@
-import pdf_plumber
+from PyPDF2 import PdfReader
 import csv
+
+arquivo_pdf = "anexos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
+arquivo_csv = "saida.csv"
+
+try:
+    reader = PdfReader(arquivo_pdf)
+
+    with open(arquivo_csv, mode="w", newline="", encoding="utf-8") as file_csv:
+        writer_csv = csv.writer(arquivo_csv)
+
+        for pagina in reader.pages:
+            texto = pagina.extract_text()
+            if texto:
+                linhas = texto.split("\n")
+                for linha in linhas:
+                    writer_csv.writerow([linha])
+
+    print("Dados salvos em: " + arquivo_csv)
+
+except IOError as e:
+    print("Ocorreu um erro: " + e)
